432719c9b04e76f78bd62c5863533af0c0c9752f
florianne-denys <r0836751@student.thomasmore.be>
2022-12-03 15:15:05 +0100

    feat: css of orders

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -9,6 +9,50 @@ import { ref } from 'vue'
         <a id="btnNewest" class="btn btn--active" href="#">Nieuwste eerst</a>
         <a id="btnOldest" class="btn" href="#">Oudste eerst</a>
     </div>
+    <div class="orders">
+      <ul >
+        <li class="order">
+            <img class="order--image" src="../assets/donut-previeuw.png" alt="donut preview">
+            <h2 class="order--title">My Donut</h2>
+            <p class="order--info">Smaak: normal</p>
+            <p class="order--info">Glazuur: none</p>
+            <p class="order--info">Topping: none</p>
+            <p class="order--info">Logo: none</p>
+        </li>
+        <li class="order">
+            <img class="order--image" src="../assets/donut-previeuw.png" alt="donut preview">
+            <h2 class="order--title">My Donut</h2>
+            <p class="order--info">Smaak: normal</p>
+            <p class="order--info">Glazuur: none</p>
+            <p class="order--info">Topping: none</p>
+            <p class="order--info">Logo: none</p>
+        </li>
+        <li class="order">
+            <img class="order--image" src="../assets/donut-previeuw.png" alt="donut preview">
+            <h2 class="order--title">My Donut</h2>
+            <p class="order--info">Smaak: normal</p>
+            <p class="order--info">Glazuur: none</p>
+            <p class="order--info">Topping: none</p>
+            <p class="order--info">Logo: none</p>
+        </li>
+        <li class="order">
+            <img class="order--image" src="../assets/donut-previeuw.png" alt="donut preview">
+            <h2 class="order--title">My Donut</h2>
+            <p class="order--info">Smaak: normal</p>
+            <p class="order--info">Glazuur: none</p>
+            <p class="order--info">Topping: none</p>
+            <p class="order--info">Logo: none</p>
+        </li>
+        <li class="order">
+            <img class="order--image" src="../assets/donut-previeuw.png" alt="donut preview">
+            <h2 class="order--title">My Donut</h2>
+            <p class="order--info">Smaak: normal</p>
+            <p class="order--info">Glazuur: none</p>
+            <p class="order--info">Topping: none</p>
+            <p class="order--info">Logo: none</p>
+        </li>
+      </ul>
+    </div>
     
  
 </template>
@@ -42,6 +86,48 @@ h1{
 .btn--active{
     color: #F1E856;
     background-color: #E72C70;
+}
+
+.orders ul{
+    display: flex;
+    flex-direction: row ;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+    padding: 0;
+    
+}
+
+.order{
+    list-style: none;
+    margin: 15px;
+    padding: 20px;
+    box-shadow: 4px 4px 30px 3px rgba(0, 0, 0, 0.25);
+    border-radius: 17px;
+    width: 220px;
+    height: 380px;
+    
+}
+
+.order--image{
+    width: 190px;
+    height: auto;
+    padding: 17px;
+}
+
+.order--title{
+    font-family: 'Dosis', sans-serif;
+    font-weight: 600;
+    font-size: 2em;
+    margin: 0;
+}
+
+.order--info{
+    font-family: 'Dosis', sans-serif;
+    font-size: 1em;
+    color: #E72C70;
+    line-height: 7px;
 }
 
 @media (min-width: 375px) {
