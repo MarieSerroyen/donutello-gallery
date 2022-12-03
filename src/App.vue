f898f84c29a83c8aa1bf7262c2a4f72822500e58
florianne-denys <r0836751@student.thomasmore.be>
2022-12-03 13:57:58 +0100

    feat: add header of page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import Navigation from './components/Nav.vue'
+import GalleryHead from './components/Head.vue'
 </script>
 
 <template>
   <Navigation/>
+      <GalleryHead/>
+    
+    
+ 
+  
 </template>
 
 <style scoped>
---
+++ src/components/Head.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <h1>Bestellingen</h1>
+    <div class="controls">
+        <a href="#">Nieuwste eerst</a>
+        <a href="#">Oudste eerst</a>
+    </div>
+    
+ 
+</template>
+
+<style scoped>
+h1{
+    font-family: 'Dosis', sans-serif;
+    padding-top: 140px;
+    text-align: center;
+}
+
+.controls{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
+
+a{
+    font-family: 'Dosis', sans-serif;
+    color: #F1E856;
+    background-color: #E72C70;
+    text-decoration: none;
+    padding: 12px 22px;
+    margin: 0 10px;
+    border-radius: 25px;
+}
+</style>
