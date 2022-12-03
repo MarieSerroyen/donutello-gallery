3c612f6ada2f0b54bd2ef1245a34d07647d2ed8b
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-03 14:05:41 +0100

    feat: add input for login form

--- src/components/LoginForum.vue
+++ src/components/LoginForum.vue
@@ -6,11 +6,29 @@
 
 <template>
     <div class="login">
-        <h1>Personeel log in</h1>
+        <h1 class="login__title">Personeel log in</h1>
+
+        <form class="login__form">
+            <label class="login__label" for="mail">E-mailadres</label>
+            <input class="login__input" type="text" id="mail" v-model="mail" required>
+
+            <label class="login__label" for="password">Wachtwoord</label>
+            <input class="login__input" type="password" id="password" v-model="password" required>
+
+            <button class="login__button" type="submit">Inloggen</button>
+        </form>
     </div>
  
 </template>
 
 <style scoped>
+    .login__title {
+        font-size: 4rem;
+    }
 
+    .login__form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
