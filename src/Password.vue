08a0ea8dfa3ba2e93d72e8145388e870af9535e4
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-03 15:34:57 +0100

    feat: import ChangeForum to Password.vue

--- src/Password.vue
+++ src/Password.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Navigation from './components/Nav.vue'
+    import ChangeForum from './components/ChangeForum.vue'
 </script>
 
 <template>
     <Navigation/>
+    <ChangeForum/>
 
 </template>
 
