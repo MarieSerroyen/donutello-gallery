fa9e441884b56eb3b60dc8ad1d35bb154efb800c
florianne-denys <r0836751@student.thomasmore.be>
2022-12-03 12:28:39 +0100

    feat: add navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+  <nav>
+    <div class="logo"></div>
+  </nav>
+
+ 
+</template>
+
+<style scoped>
+nav {
+  background-color: #E72C70;
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 79px;
+  width: 100%;
+}
+.logo{
+  color: white;
+  background-image: url('../assets/donuttello-logo.png');
+  background-size: 130px;
+  height: 100px;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding-top: 25px;
+}
+</style>
