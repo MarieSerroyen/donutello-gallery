8570e95ac0661e5ed0b3f6be9302129d8cbae35a
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-12 14:44:28 +0100

    feat: add routes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
+})
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'login.html'),
+        changepassword: resolve(__dirname, 'wijzig_wachtwoord.html')
+      }
+    }
+  }
 })
