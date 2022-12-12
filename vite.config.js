d3b5600eb86a8eaac41506579e12e61de97857d7
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-12 14:56:53 +0100

    feat: remove vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'login.html'),
-        changepassword: resolve(__dirname, 'wijzig_wachtwoord.html')
-      }
-    }
-  }
+  plugins: [vue()]
 })
