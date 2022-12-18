d83492bbab3b165d73c1715a08eb82ca7c459f5f
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-18 08:59:18 +0100

    feat: remove unused functions

--- src/main.js
+++ src/main.js
@@ -4,25 +4,3 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-// event listener for the button
-document.getElementById('btnNewest').addEventListener('click', () => {
-    console.log('btnNewest clicked')
-    if (!document.getElementById('btnNewest').classList.contains('btn--active')) {
-        document.getElementById('btnNewest').classList.add('btn--active')
-    }
-    if (document.getElementById('btnOldest').classList.contains('btn--active')) {
-        document.getElementById('btnOldest').classList.remove('btn--active')
-    }
-    
-})
-
-document.getElementById('btnOldest').addEventListener('click', () => {
-    console.log('btnOldest clicked')
-    if (!document.getElementById('btnOldest').classList.contains('btn--active')) {
-        document.getElementById('btnOldest').classList.add('btn--active')
-    }
-    if (document.getElementById('btnNewest').classList.contains('btn--active')) {
-        document.getElementById('btnNewest').classList.remove('btn--active')
-    }
-    
-})
